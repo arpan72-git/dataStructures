eb6d46cfe79de0aef5b189382e9c6af83cc1db40
Arpan Agrawal <arpan.agrawal@nagarro.com>
2021-11-23 12:31:20 +0530

    moved  projects to separate repo

--- MutithreadingExample/src/main/java/com/example/multithreading/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.multithreading.repository;
-
-import com.example.multithreading.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-}
